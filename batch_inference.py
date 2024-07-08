30ff902576f84657d1e53a134837b14fe4c4a438
Ali <vynz420@gmail.com>
2024-07-09 04:02:12 +0500

    add . for windows

--- batch_inference.py
+++ batch_inference.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # please write your input folder name here
     # line 12
     USER_INPUT_FOLDER = ''
-    file_names = glob.glob(f"/input/{USER_INPUT_FOLDER}/*.mp3")
+    file_names = glob.glob(f"./input/{USER_INPUT_FOLDER}/*.mp3")
  
     prompt = "And this is a conversation between Seller and Customer. Seller is selling insurance or something like that. There can be more than one sellers in conversation."
     model = DiarizationPipeline(prompt=prompt,out_model=ConversationList)
